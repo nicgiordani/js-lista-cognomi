7c5890a1f54690fa0eac9fa57ca24d1e9cc5279c
nicgiordani <nikgiordani@gmail.com>
2020-11-16 15:28:31 +0100

    inizio

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Chiedere all’utente il cognome,
+// inserirlo in un array con altri cognomi (Es: 'Pesce', ‘Castenetto’, 'Passaro', 'Grimaldi', ‘Rossi’, ‘Verdi’, ‘Bianchi’).
+// Stampa la lista ordinata alfabeticamente.
+// Scrivi anche la posizione "umana" della lista in cui il nuovo utente si trova.
+
+var cognome = prompt("inserisci il tuo cognome");
+// console.log(cognome);
+
+var listaCognomi = ["Pesce", "Castanetto", "Passaro", "Grimaldi", "Rossi", "Verdi", "Bianchi",]
+
+for(var i = 0; i < listaCognomi.length; i++) {
+  console.log(listaCognomi[i]);
+}
